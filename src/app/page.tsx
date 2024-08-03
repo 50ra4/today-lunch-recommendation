ac9277fd6b73690af087810285777112b9e8264c
50ra4 <50ra4.dev.dev@gmail.com>
2024-08-03 16:51:52 +0900

    refactor: SideMenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 'use client';
 
-import {
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  useDisclosure,
-} from '@chakra-ui/react';
+import { useDisclosure } from '@chakra-ui/react';
 
 import { ChatList } from '@/components/ChatList';
 import { Header } from '@/components/Header';
-import { HistoryCard } from '@/components/HistoryCard';
 import { PreferenceModal } from '@/components/PreferenceModal';
-import { CHATS, HISTORIES } from '@/mock';
+import { SideMenu } from '@/components/SideMenu';
+import { CHATS } from '@/mock';
 
 export default function Home() {
   const drawer = useDisclosure();
@@ -25,18 +17,7 @@ export default function Home() {
       <Header title="ランチ君" onClickHistory={drawer.onOpen} onClickPreference={modal.onOpen} />
       <main className="min-h-screen ">
         <ChatList chats={CHATS} />
-        <Drawer onClose={drawer.onClose} isOpen={drawer.isOpen} size="md" placement="left">
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>検索履歴</DrawerHeader>
-            <DrawerBody>
-              {HISTORIES.map(({ id, searchedAt, detail }) => (
-                <HistoryCard key={id} id={id} searchedAt={searchedAt} detail={detail} />
-              ))}
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
+        <SideMenu isOpen={modal.isOpen} onClose={modal.onClose} />
         <PreferenceModal isOpen={modal.isOpen} onClose={modal.onClose} />
       </main>
     </>
